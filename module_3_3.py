a69d7c47d5570eb329c52a48a0ff0cf99f1a17c7
Elena-1717 <kea5599@mail.ru>
2024-07-16 13:48:14 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,15 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params(b=25, c=[1, 2, 3])
+
+
+values_list = ['Hello', 3, True]
+values_dict = {'a': 1, 'b': 'строка', 'c': True}
+print_params(*values_list)
+print_params(**values_dict)
+
+
+values_list_2 = [62, 'строка']
+print_params(*values_list_2, 42)
